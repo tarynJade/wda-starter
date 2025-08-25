8c9abb66f3b840a1ed1edf2ebb6d94192c26c288
Tom Parson <tom@workwithhuddle.com>
2025-08-25 11:40:39 +0100

    Initial commit

---
+++ js/movies.js
@@ -0,0 +1,15 @@
+export default class MovieHelper {
+
+    constructor() {
+        // Define our API root URL, we can then add specific paths onto the end for different queries
+        this.api_root = "https://api.themoviedb.org/3"
+        // Define our API key here
+        this.api_key = "YOUR_API_KEY"
+    }
+
+    // Use this API endpoint: https://developer.themoviedb.org/reference/discover-movie
+    async getMovies() {
+        // return ...
+    }
+
+}
