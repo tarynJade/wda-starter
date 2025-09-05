071ed9f20b2d2807f10c3512b2cb653349319874
Tom Parson <tom@workwithhuddle.com>
2025-09-05 14:45:15 +0100

    Adding movie page

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,12 @@ async function loadMovieHelper() {
     return MovieHelper
 }
 
+// Helper function to get parameter from URL
+function getUrlParam(param) {
+  const urlParams = new URLSearchParams(window.location.search)
+  return urlParams.get(param)
+}
+
 let movieListComponent = {
   movies: [],
   filter_year: '',
@@ -25,5 +31,23 @@ let movieListComponent = {
     // If filter_year is set, change what movies we load from the API
     // You could do this by calling a different method, or passing arguments into getMovies()
     this.movies = await MovieHelper.getMovies()
+  }
+}
+
+let movieComponent = {
+  movie: null,
+  init() {
+    // Get movie parameter from URL that looks like this
+    //     movie.html?movie_id=456
+    // Add links to your index.html to point to movie.html?movie_id={your_movie_id}
+    const movie_id = getUrlParam('movie_id')
+
+    if (movie_id) {
+      this.loadMovie(movie_id)
+    }
+  },
+  async loadMovie(movie_id) {
+    // Load actual movie data from API using movie_id
+    this.movie = movie_id
   }
 }
