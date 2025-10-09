67a504f6d659100a3b3001fda00b2313f181185b
tarynJade <taryn.bilsborough@moneysupermarket.com>
2025-10-09 17:35:36 +0100

    refactor: move Tailwind CSS configuration to a separate file and update Content-Security-Policy

---
+++ js/tailwindConfig.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              light: "#F5FBEF",
+              salmon: "#FD96A9",
+              navy: "#444B6E",
+              cerise: "#D0465F",
+            },
+          },
+        },
+      };
